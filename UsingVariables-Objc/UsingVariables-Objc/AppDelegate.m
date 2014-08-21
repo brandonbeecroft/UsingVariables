72733318c3a97d8ad64bfbd994411369706ed3c0
Brandon Beecroft <brandonbeecroft@gmail.com>
2014-08-20 21:43:15 -0600

    add bodyMassIndex  calculation

--- UsingVariables-Objc/UsingVariables-Objc/AppDelegate.m
+++ UsingVariables-Objc/UsingVariables-Objc/AppDelegate.m
@@ -18,7 +18,16 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
     // Insert code here...
-    
+    CGFloat height;
+    CGFloat weight;
+    CGFloat bodyMassIndex;
+
+    height = 71.0;
+    weight = 185.0;
+
+    bodyMassIndex = (weight / (height * height) * 703);
+
+    NSLog(@"Body mass index is: %f", bodyMassIndex);
     
     return YES;
 }
